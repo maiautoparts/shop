96d994b1a2211ed6992e2d9d024408d0fcf3cdb7
Daniel Túnez Bermejo <daniel.tunez95@gmail.com>
2016-06-07 17:48:57 +0200

    reparacion

--- Sincronización/Productos.cs
+++ Sincronización/Productos.cs
@@ -12,7 +12,7 @@ namespace Sincronización
 {
     public class Productos : Base
     {
-        private string connectionStringtest = GetConnectionString();
+        private string connectionString = GetConnectionString();
         private ProductoLista.ProductoLista_Service servLista;
         private ProductoFicha.ProductoFicha_Service servFicha;
         private ProductoImagen.ProductoImagen_Service servImagen;
